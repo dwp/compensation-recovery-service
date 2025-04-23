0cd8e38eb01ed88f5e2dfd5b1332a0b296c840a7
Nikhil Kadiamada Kushalappa <nikhilkadiamada.kushalappa@engineering.digital.dwp.gov.uk>
2025-04-23 13:17:25 +0100

    feat: removed unused layouts + cleaned code

--- app/filters.js
+++ app/filters.js
@@ -2,25 +2,26 @@
 // For guidance on how to create filters see:
 // https://prototype-kit.service.gov.uk/docs/filters
 //
-
+const { DateTime } = require('luxon')
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
-var filters = {}
+let filters = {}
 
 /* ------------------------------------------------------------------
   keep the following line to return your filters to the app
 ------------------------------------------------------------------ */
 
+filters.baseDate = function(format,num) {
+  const daysToReduce = Math.ceil(num);
+  const date = DateTime.now().minus({ days: daysToReduce }).toFormat(format)
+  return date.isValid ? date : ''
+}
+
 filters.toMonth = function(x) {
-  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];   
-  if (x > 0){ return months[x - 1]; // returns date as per month      
-  } else {
-          return x ;      
-  }}     
-  filters.toMoney = function(x){  return("£" + x );
-  //TO ADD - case to handle nothing being there 
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  return x > 0 ? months[x - 1]: x
 }
 
 // Add the filters using the addFilter function
